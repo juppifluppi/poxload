e92b5b9b3e8605dedb614f9b35decb738219f314
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-18 12:41:04 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -58,7 +58,7 @@ with open("descriptors_mordred_fda.csv","a") as f:
     for o in ax.values():
         f.write(str(o)+",")
         f.write("\n")
-  
+st.write(descriptors)  
 st.image(im)
 
 #except:
