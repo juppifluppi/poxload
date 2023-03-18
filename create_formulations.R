fec8c70db8169d8b7736adadd5b43de0d3acdfe7
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-18 13:26:42 +0000

    Update create_formulations.R

--- create_formulations.R
+++ create_formulations.R
@@ -3,11 +3,11 @@ padelpol=read.csv("descriptors_padel_pol.csv")
 mordred=read.csv("descriptors_mordred_fda.csv")
 mordredpol=read.csv("descriptors_mordred_pol.csv")
 
-print(ncol(padelpol))
-
 padel=cbind(padel[1:nrow(padel)/2,1],padel[((nrow(padel)/2)+1):nrow(padel),1])
 padel=t(padel)
 
+print(ncol(padel))
+print(ncol(padelpol))
 
 #mordredpol=mordredpol[,colnames(mordredpol)%in%colnames(mordred)]
 #mordred=mordred[,colnames(mordred)%in%colnames(mordredpol)]
