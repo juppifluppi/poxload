448031f643969c7725648e8ffca50f338697d4ee
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-19 10:55:38 +0000

    Update create_formulations.R

--- create_formulations.R
+++ create_formulations.R
@@ -9,6 +9,8 @@ x=c("Drug",x)
 names(x)=c("Name",(padel[c(1:(nrow(padel)/2)),1]))
 padelpol=padelpol[,colnames(padelpol)%in%names(x)]
 
+print(x)
+
 padel=rbind(x,padelpol)
 
 x=(mordred[c(((nrow(mordred)/2)+1):(nrow(mordred))),1])
