1d8189ad7b2075a29ee3ef4deffcbd4867babeec
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-19 13:06:47 +0000

    Update predict.R

--- predict.R
+++ predict.R
@@ -12,9 +12,13 @@ colnames(fda)[ncol(fda)]="formulations.Time"
 
 axb=fda$POL
 
+print(fda[1,])
+
 for(i in c(1:ncol(fda))){
   fda[,i]=(as.numeric(as.character(fda[,i]))
 }
+           
+print(fda[1,])
 
 fx=predict(final_model[["cubist"]],fda,na.action=na.pass)
 iji=cbind(axb,iji)
