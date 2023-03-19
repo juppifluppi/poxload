482b4d3c3e415794c68b449b3dab1d42df0d6247
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-19 00:00:10 +0000

    Update predict.R

--- predict.R
+++ predict.R
@@ -19,6 +19,5 @@ for(i in c(1:ncol(fda))){
 fx=predict(final_model[["cubist"]],fda,na.action=na.pass)
 iji=cbind(axb,iji)
 colnames(iji)=c("Polymer","LE (%)")
-df <- df %>%
-  filter(duplicated(Polymer) == FALSE
+iji=iji[-24,]
 write.csv(iji,"fx.csv",row.names=F)
