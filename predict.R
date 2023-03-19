561a2bbf5c22a3207e7170bd5ff9dc6d6c8acff8
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-19 13:17:22 +0000

    Update predict.R

--- predict.R
+++ predict.R
@@ -3,6 +3,9 @@ devtools::load_all("Cubist")
 load("cubist.rda")
 
 fda=read.csv("fdaset.dat")
+
+print(fda[1,])
+
 fg=rep(10,nrow(fda))
 fz=rep(0,nrow(fda))
 fda=cbind(fda,fg)
@@ -12,14 +15,10 @@ colnames(fda)[ncol(fda)]="formulations.Time"
 
 axb=fda$POL
 
-print(fda[1,])
-
 for(i in c(1:ncol(fda))){
   fda[,i]=(as.numeric(as.character(fda[,i]))
 }
            
-print(fda[1,])
-
 fx=predict(final_model[["cubist"]],fda,na.action=na.pass)
 iji=cbind(axb,iji)
 colnames(iji)=c("Polymer","LE (%)")
