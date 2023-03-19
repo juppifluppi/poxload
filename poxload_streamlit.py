21650fc10a8dee4b604504f5af28640c84ca3b7f
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-19 00:44:43 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -32,7 +32,6 @@ dimorphite_dl = DimorphiteDL(
 
 #try:
 SMI = st.text_input('Enter SMILES code of drug to load', 'CC1=C2[C@@]([C@]([C@H]([C@@H]3[C@]4([C@H](OC4)C[C@@H]([C@]3(C(=O)[C@@H]2OC(=O)C)C)O)OC(=O)C)OC(=O)c5ccccc5)(C[C@@H]1OC(=O)[C@H](O)[C@@H](NC(=O)c6ccccc6)c7ccccc7)O)(C)C')  
-write
 # SMI = str(dimorphite_dl.protonate(SMI)[0])    
 mol = Chem.MolFromSmiles(SMI)
 sdm = pretreat.StandardizeMol()
