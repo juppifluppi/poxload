a126830cd2afbd7b29d0d0341e17257960a91730
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-19 12:32:43 +0000

    Update create_formulations.R

--- create_formulations.R
+++ create_formulations.R
@@ -10,15 +10,14 @@ names(x)=c("Name",(padel[c(1:(nrow(padel)/2)),1]))
 padelpol=padelpol[,colnames(padelpol)%in%names(x)]
 af=names(x)
 
-print(nrow(padel))
-print(nrow(padelpol))
-
 x=data.frame(matrix(x, 1))
 colnames(x)=names(x)
 padel=rbind(padelpol,x)
 colnames(padel)=af
 padel=padel[,-2]
 
+print(nrow(padel))
+print(ncol(padel))
 
 
 x=as.numeric(as.character((mordred[c(((nrow(mordred)/2)+1):(nrow(mordred))),1])))
@@ -27,15 +26,14 @@ names(x)=c("Name",(mordred[c(1:(nrow(mordred)/2)),1]))
 mordredpol=mordredpol[,colnames(mordredpol)%in%names(x)]
 af=names(x)
 
-print(nrow(mordred))
-print(nrow(mordredpol))
-
-
 x=data.frame(matrix(x, 1))
 colnames(x)=names(x)
 mordred=rbind(mordredpol,x)
 colnames(mordred)=af
 mordred=mordred[,-2]
+
+print(nrow(mordred))
+print(ncol(mordredpol))
 
 
 
