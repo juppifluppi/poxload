3de717cdef01be8a2a1b833c4831ebffc35d5e9f
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-19 01:02:19 +0000

    Update create_formulations.R

--- create_formulations.R
+++ create_formulations.R
@@ -4,6 +4,8 @@ padelpol=read.csv("descriptors_padel_pol.csv")
 mordred=read.csv("descriptors_mordred_fda.csv")
 mordredpol=read.csv("descriptors_mordred_pol.csv")
 
+print(padel)
+
 x=(padel[c(((nrow(padel)/2)+1):(nrow(padel))),1])
 x=c("Drug",x)
 names(x)=c("Name",(padel[c(1:(nrow(padel)/2)),1]))
