7e37913fcadd892385a7ecb295d0a45fb385b00c
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-03-20 10:05:21 +0000

    Update predict.R

--- predict.R
+++ predict.R
@@ -4,7 +4,7 @@ load("cubist.rda")
 
 fda=read.csv("fdaset.dat")
 
-fg=rep(2,nrow(fda))
+fg=rep(10,nrow(fda))
 fz=rep(0,nrow(fda))
 fda=cbind(fda,fg)
 
@@ -18,7 +18,7 @@ fx=predict(final_model[["cubist"]],fda,na.action=na.pass)
 fx=round(fx,0)
 iji=cbind(axb,fx)
 
-fda$formulations.DF=rep(4,nrow(fda))
+fda$formulations.DF=rep(8,nrow(fda))
 fx=predict(final_model[["cubist"]],fda,na.action=na.pass)
 fx=round(fx,0)
 iji=cbind(iji,fx)
@@ -28,17 +28,17 @@ fx=predict(final_model[["cubist"]],fda,na.action=na.pass)
 fx=round(fx,0)
 iji=cbind(iji,fx)
 
-fda$formulations.DF=rep(8,nrow(fda))
+fda$formulations.DF=rep(4,nrow(fda))
 fx=predict(final_model[["cubist"]],fda,na.action=na.pass)
 fx=round(fx,0)
 iji=cbind(iji,fx)
 
-fda$formulations.DF=rep(10,nrow(fda))
+fda$formulations.DF=rep(2,nrow(fda))
 fx=predict(final_model[["cubist"]],fda,na.action=na.pass)
 fx=round(fx,0)
 iji=cbind(iji,fx)
 
 iji=iji[-c(20,26),]
 
-colnames(iji)=c("Polymer","LE2","LE4","LE6","LE8","LE10")
+colnames(iji)=c("Polymer","LE10","LE8","LE6","LE4","LE2")
 write.csv(iji,"fx.csv",row.names=F)
