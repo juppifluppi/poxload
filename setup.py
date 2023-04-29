df1314fef3370e522e1a7ea16130392fd79a9327
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-04-29 13:54:30 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,52 +1,56 @@
 from setuptools import find_packages, setup
 
-__version__ = "1.4"
+__version__ = "1.0"
 
 # Load README
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='tclint',
-    version='1.4',
+    name='poxload',
+    version='1.0',
     author='Josef Kehrein',
     author_email='josef.kehrein@helsinki.fi',
     license='MIT',
-    description='Predict interaction of molecule with lecithin/taurocholate',
+    description='Predict drug loading for poly(2-oxazoline) micelles',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/juppifluppi/tclint',
-    download_url=f'https://github.com/juppifluppi/tclint/archive/refs/tags/v_{__version__}.tar.gz',
+    url='https://github.com/juppifluppi/poxload',
+    download_url=f'https://github.com/juppifluppi/poxload/archive/refs/tags/v_{__version__}.tar.gz',
     project_urls={
-        'Source': 'https://github.com/juppifluppi/tclint',
-        'Web application': 'http://tclint.streamlit.app',
+        'Source': 'https://github.com/juppifluppi/poxload',
+        'Web application': 'http://poxload.streamlit.app',
     },
     packages=find_packages(),
-    package_data={'tclint': ['py.typed'],
+    package_data={'poxload': ['py.typed'],
     '': ['csv.typed']},
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'tclint=tclint',
+            'poxload=poxload',
+            'poxload_batch=poxload_batch'
         ]
     },
     install_requires=[
-        'numpy>=1.23.4',        
-        'dimorphite-dl>=1.3.2',
-        'rdkit>=2022.09.1',    
-        'scopy>=1.2.5'         
+        'numpy==1.19.0',        
+        'rdkit==2022.09.5',    
+        'scopy>=1.2.5',
+        'jazzy==0.0.11',
+        'padelpy',
+        'sirms',
     ],
     python_requires='>=3.7',
     classifiers=[
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: R :: 4.2.2',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent'
     ],
     keywords=[
         'chemistry',
         'property prediction',
-        'logistic regression'
+        'random forest'
     ]
 )
 
