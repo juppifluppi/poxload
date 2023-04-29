67cb2c14fb97b7dcf39f14f34d2b188fefc99a69
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-04-29 23:06:53 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -54,11 +54,7 @@ option = st.selectbox(
     'What model would you like to use?',
     ('PaDEL (fast)', 'PaDEL+SIRMS (slower)'))
 
-with st.form(key='my_form_to_submit'):
-    ...
-    submit_button = st.form_submit_button(label='Submit')
-
-if submit_button:
+if len(SMI)>0:
     
     if option == "PaDEL+SiRMS (slower)":
         with st.spinner('CALCULATING PADEL DESCRIPTORS FOR COMPOUND (STEP 1 OF 6)...'):
