552986b14643f77a42f1dc1e162a395b1ff62b7b
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-04-29 22:57:37 +0000

    Update fgv.R

--- fgv.R
+++ fgv.R
@@ -4,7 +4,7 @@ af2=read.csv("descp.csv")
 af3=read.csv("sirms_test.txt",check.names = F)
 colnames(af3)[1]="Mixture"
 afx=cbind(af,af2,af3)
-ui=read.csv("startdatayyy3.dat",check.names = F)
+ui=read.csv("startdatayyy6.dat",check.names = F)
 afx=afx[,colnames(afx)%in%colnames(ui)]
 Missing <- setdiff(colnames(ui), colnames(afx))
 afx[Missing] <- 0
@@ -13,37 +13,45 @@ afx <- afx[colnames(ui)]
 a=af$POL
 a=as.data.frame(a)
 
-load("m1.rda")
+load("m1v.rda")
 b=unlist(predict(final_model,newdata=afx))
 a=cbind(a,b)
 
-load("m2.rda")
+load("m2v.rda")
 b=unlist(predict(final_model,newdata=afx))
 a=cbind(a,b)
 
-load("m3.rda")
+load("m3v.rda")
 b=unlist(predict(final_model,newdata=afx))
 a=cbind(a,b)
 
-load("m4.rda")
+load("m35v.rda")
 b=unlist(predict(final_model,newdata=afx))
 a=cbind(a,b)
 
-load("m5.rda")
+load("m4v.rda")
 b=unlist(predict(final_model,newdata=afx))
 a=cbind(a,b)
 
-load("m6.rda")
+load("m5v.rda")
 b=unlist(predict(final_model,newdata=afx))
 a=cbind(a,b)
 
-load("m7.rda")
+load("m6v.rda")
 b=unlist(predict(final_model,newdata=afx))
 a=cbind(a,b)
 
-load("m8.rda")
+load("m7v.rda")
 b=unlist(predict(final_model,newdata=afx))
 a=cbind(a,b)
 
-colnames(a)=c("POL","LC10","LC20","LC30","LC40","LE20","LE40","LE60","LE80")
+load("m75v.rda")
+b=unlist(predict(final_model,newdata=afx))
+a=cbind(a,b)
+
+load("m8v.rda")
+b=unlist(predict(final_model,newdata=afx))
+a=cbind(a,b)
+
+colnames(a)=c("POL","LC10","LC20","LC30","LC35","LC40","LE20","LE40","LE60","LE70","LE80")
 write.csv(a,"fin_results.csv",row.names=F)
