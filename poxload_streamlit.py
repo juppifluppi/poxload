9f2af66139434494c4ead79321312cb29ec7ce6e
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-04-29 13:36:10 +0300

    Add files via upload

---
+++ poxload_streamlit.py
@@ -0,0 +1,207 @@
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import ChemicalFeatures
+from rdkit.Chem import Crippen
+from jazzy.api import molecular_vector_from_smiles
+from jazzy.api import atomic_map_from_smiles
+from jazzy.api import atomic_tuples_from_smiles
+from jazzy.api import deltag_from_smiles
+from jazzy.api import atomic_strength_vis_from_smiles
+from jazzy.core import rdkit_molecule_from_smiles, kallisto_molecule_from_rdkit_molecule
+from jazzy.core import get_covalent_atom_idxs, get_charges_from_kallisto_molecule, calculate_polar_strength_map
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
+from dimorphite_dl import DimorphiteDL
+from rdkit.Chem.Fingerprints import FingerprintMols
+from scopy.ScoPretreat import pretreat
+import scopy.ScoDruglikeness
+from padelpy import from_smiles
+import sys, os
+from mordred import Calculator, descriptors
+from io import StringIO
+from rdkit import RDConfig
+from jazzy.core import rdkit_molecule_from_smiles
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
+from rdkit.Chem.Fingerprints import FingerprintMols
+from dimorphite_dl import DimorphiteDL
+from rdkit.Chem.Fingerprints import FingerprintMols
+from scopy.ScoPretreat import pretreat
+import scopy.ScoDruglikeness
+from padelpy import from_smiles
+import sys, os
+from mordred import Calculator, descriptors
+import pandas as pd
+import time 
+
+import streamlit as st
+import time
+import subprocess
+from PIL import Image
+
+st.image('logo.png')
+
+#st.title('POxload prediction model')
+
+st.caption("*** WORK IN PROGRESS ***")
+
+st.caption("""Input a [molecule SMILES code](https://pubchem.ncbi.nlm.nih.gov/edit3/index.html). Predictions for loading efficencies 
+for different ABA-triblock poly(2-oxazoline)- and poly(2-oxazine)-based micelles are listed, given a polymer feed of 10 g/L and drug feeds of
+10, 8, 6, 4 or 2 g/L. A cubist regression model for predictions of continuous LE values in % and a treebag classification model for probabilities of LE ≥ 80% are listed.""")
+
+SMI = st.text_input('Enter SMILES code of drug to load', '')  
+
+with st.spinner('Computing loading efficiencies, please wait...'):
+    NAME = "testcompound"
+    
+    mol = Chem.MolFromSmiles(SMI)
+    sdm = pretreat.StandardizeMol()
+    mol = sdm.disconnect_metals(mol)
+    SMI = str(Chem.MolToSmiles(mol))
+    
+    #print("CALCULATING PADEL DESCRIPTORS FOR COMPOUND...")
+    
+    descriptors = from_smiles(SMI)
+    items = list(descriptors.items())
+    descriptors = dict(items)
+    items.insert(0, ('Name', NAME))
+    
+    with open("descriptors_padeltest.csv","w") as f:
+        for o in descriptors.keys():
+            f.write(str(o)+",")
+        f.write("\n")
+    
+    with open("descriptors_padeltest.csv","a") as f:
+        for o in descriptors.values():
+            f.write(str(o)+",")
+    
+    mols=[]
+    sum_MW=[]
+    sum_SMILES=[]
+    sum_NAME=[]
+    
+    sum_SMILES.append(str(sys.argv[1]))
+    sum_NAME.append(NAME)
+    
+    #print("CALCULATING ATOMIC PROPERTIES FOR SiRMS...")
+            
+    mol = rdkit_molecule_from_smiles(SMI, minimisation_method="MMFF94")
+    kallisto_mol = kallisto_molecule_from_rdkit_molecule(mol)
+    atoms_and_nbrs = get_covalent_atom_idxs(mol)
+    kallisto_charges = get_charges_from_kallisto_molecule(kallisto_mol, charge=0)  
+    a = calculate_polar_strength_map(mol, kallisto_mol, atoms_and_nbrs, kallisto_charges)
+    
+    contribs = Crippen.rdMolDescriptors._CalcCrippenContribs( mol )    
+    logps, mrs = zip( *contribs )
+        
+    comp = 0
+    for atom in mol.GetAtoms():
+        comp += 1
+    
+    for ji in ["eeq","alp","num_lp","sdc","sdx","sa"]:
+        for nmx in range(0,comp):
+            mol.GetAtomWithIdx(nmx).SetProp(ji,str(str(a[nmx][ji])+";"))
+            Chem.CreateAtomStringPropertyList(mol,ji,lineSize=99999)
+        for nmx in range(0,comp):
+            mol.GetAtomWithIdx(nmx).SetProp("at",str(str(a[nmx]["z"])+">"+str(a[nmx]["hyb"])+";"))
+            Chem.CreateAtomStringPropertyList(mol,"at",lineSize=99999)    
+        if str(a[nmx]["hyb"]) == "unspecified":
+            mol.GetAtomWithIdx(nmx).SetProp("at",str(str(a[nmx]["z"])+">s;"))
+            Chem.CreateAtomStringPropertyList(mol,"at",lineSize=99999)    
+                
+                
+    for nmx in range(0,comp):            
+        mol.GetAtomWithIdx(nmx).SetProp("logp",str(str(logps[nmx])+";"))
+        Chem.CreateAtomStringPropertyList(mol,"logp",lineSize=99999)    
+    for nmx in range(0,comp):            
+        mol.GetAtomWithIdx(nmx).SetProp("mr",str(str(mrs[nmx])+";"))
+        Chem.CreateAtomStringPropertyList(mol,"mr",lineSize=99999)
+            
+    mol.SetProp('_Name',NAME)
+                
+    mols.append(mol)
+    
+    mj = Chem.Descriptors.ExactMolWt(mol)
+    sum_MW.append(mj)
+        
+    with Chem.SDWriter('librarytest.sdf') as w:
+        for m in mols:
+            w.write(m)
+    
+    os.system("sed -i -e 's/atom.prop.//g' librarytest.sdf")
+    os.system("sed -i -e 's/; /;/g' librarytest.sdf")
+    os.system("cat librarytest.sdf library_pol.sdf > db_library_merged.sdf")
+    
+    dfx = pd.DataFrame(columns=['NAME', "SMILES","MW"])
+    dfx["NAME"]=sum_NAME
+    dfx["SMILES"]=sum_SMILES
+    dfx["MW"]=sum_MW
+    
+    dfx.to_csv("db_molstest.csv",index=False)
+    
+    #print("CREATING FORMULATIONS...")
+    
+    process1 = subprocess.Popen(["Rscript", "cxdb.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result1 = process1.communicate()
+    
+    #os.system("Rscript cxdb.R")
+    
+    os.system("sed -i -e 's/\"//g' formulations3test_db.csv")
+    
+    try:
+        os.remove("mixture_test.txt")
+    except:
+        pass
+    
+    with open("formulations3test_db.csv","r") as f:
+        lines=f.readlines()
+        for o in lines[1:]:
+            o=o.split("\t")
+            with open("mixture_test.txt", "a") as file:
+                file.write(str(o[-6])+"\t"+str(o[-5]))
+                if str(o[-4]) != "None":
+                    file.write("\t"+str(o[-4]))
+                file.write("\t"+str(o[-3])+"\t"+str(o[-2]))
+                if float(o[-1]) > 0:
+                    file.write("\t"+str(o[-1]))
+                else:
+                    file.write("\n")
+    
+    #print("CALCULATING SiRMS DESCRIPTORS...")
+    
+    os.system("sirms -i db_library_merged.sdf -a mr logp eeq alp sa sdx sdc at -o sirms_test.txt -m mixture_test.txt --max_mix_components 3 --mix_type rel -c 1 -r > /dev/null 2>&1")
+    
+    os.system("sed -i -e 's/\t/,/g' sirms_test.txt")
+    
+    #print("CALCULATING PADEL DESCRIPTORS FOR MIXTURES...")
+    process2 = subprocess.Popen(["Rscript", "gtg.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result2 = process2.communicate()
+    #os.system("Rscript gtg.R > /dev/null 2>&1")
+    #print("CALCULATING PREDICTIONS...")
+    process3 = subprocess.Popen(["Rscript", "fgv.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result3 = process3.communicate()
+    #os.system("Rscript fgv.R > /dev/null 2>&1")
+    #print("WRITE RESULTS TO CSV...")
+    #print("DONE! CALCULATION TIME: {0} SECONDS".format(time.time() - startTime))
+    
+    df = pd.read_csv(r'fin_results.csv',index_col=0)
+    df = df.rename(columns={0: "Polymer", 1: "LC10", 2: "LC20", 3: "LC30", 4: "LC40", 5: "LE20", 6: "LE40", 7: "LE60", 8: "LE80"})
+    #df = df.sort_values('LE10',ascending=False)
+    df2 = df
+    
+    col1, col2 = st.columns(2)
+    with col1:
+        st.caption("""Cubist regression (RMSE = 15 %)""")
+        st.dataframe(df1.style.hide_index().background_gradient(axis=None, vmin=50, vmax=100, cmap="Reds"),height=212)
+        st.caption("""Treebag probabilities for LE ≥ 80% (BAcc = 91 %)""")
+        st.dataframe(df2.style.hide_index().background_gradient(axis=None, vmin=50, vmax=100, cmap="Reds"),height=212)
+    with col2:
+        st.image(im)
+    
+# reference
+
+st.caption("[github page](https://github.com/juppifluppi/poxload)")
