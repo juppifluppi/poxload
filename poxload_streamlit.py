4fd3c20b3486eb9fa0d42fbe6b635b63acc32dda
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-04-29 13:30:00 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -43,9 +43,10 @@ st.image('logo.png')
 
 st.caption("*** WORK IN PROGRESS ***")
 
-st.caption("""Input a [molecule SMILES code](https://pubchem.ncbi.nlm.nih.gov/edit3/index.html). Predictions for loading efficencies 
-for different ABA-triblock poly(2-oxazoline)- and poly(2-oxazine)-based micelles are listed, given a polymer feed of 10 g/L and drug feeds of
-10, 8, 6, 4 or 2 g/L. A cubist regression model for predictions of continuous LE values in % and a treebag classification model for probabilities of LE ≥ 80% are listed.""")
+st.caption("""Input a [molecule SMILES code](https://pubchem.ncbi.nlm.nih.gov/edit3/index.html). Predictions for loading efficencies (LE) and
+loading capacities (LC) for different ABA-triblock poly(2-oxazoline)- and poly(2-oxazine)-based micelles are listed, given a polymer feed of 10 g/L and a drug feed of 8 g/L.
+Random forest classifications for different thresholds are listed (LE ≥ 20, 40, 60, 70 and 80%; LC ≥ 10, 20, 30, 35 and 40%).
+Mixtures that exceed these thresholds are labeled "X1" and colored green.""")
 
 SMI = st.text_input('Enter SMILES code of drug to load', '')  
 
@@ -193,10 +194,10 @@ with st.spinner('CALCULATING PREDICTIONS (STEP 6 OF 6)...'):
     
     col1, col2 = st.columns(2)
     with col1:
-        st.dataframe(df1.style.applymap(cooling_highlight))
+        st.dataframe(df2.style.applymap(cooling_highlight))
         st.image(im)
     with col2:
-        st.dataframe(df2.style.applymap(cooling_highlight))
+        st.dataframe(df1.style.applymap(cooling_highlight))
 
     
 # reference
