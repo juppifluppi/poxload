8dc3467c9b36754be51a8e889867f8ad995b5c87
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-07 11:39:22 +0000

    Update cxdb.R

--- cxdb.R
+++ cxdb.R
@@ -1,32 +1,31 @@
-dr=read.csv("db_molstest.csv")
-formulations=read.csv("smiles3.csv",dec=".")
-fill=read.csv("smiles3.csv")
+dr=read.csv("descriptors_rdk7.csv",check.names = F,sep = "\t")
+test=read.csv("db_test.csv")
+fill=read.csv("db_smiles.csv")
+dr=cbind(test,dr)
 
 fill$DF=8
 fill$D2="None"
 fill$DF2=0
 fill$DRUGNAME2="None"
+fill$SD2="None"
 fill$DMW2=0
 fill$MR2=0
 fill$D2FRAC=0
-
-#fill=fill[fill$POL%in%c("A-nPrOzi-A","A-nBuOx-A","A-nBuOzi-A","A-PentOx-A","A-PhOx-A-H","A-BzOx-A-H","A-nPrOx-A"),]
-#fill[fill$POL=="A-BzOx-A-H",1]=="A-BzOx-A"
-#fill[fill$POL=="A-PhOx-A-H",1]=="A-PhOx-A"
 
 full=rep(NA,ncol(fill))
 
 for(i in c(1:nrow(dr))){
   fillx=fill
+  fillx$Source=dr[i,colnames(dr)%in%"NAME"]
   fillx$D=dr[i,colnames(dr)%in%"NAME"]
   fillx$DMW=dr[i,colnames(dr)%in%"MW"]
   fillx$DRUGNAME1=dr[i,colnames(dr)%in%"NAME"]
   fillx$MR1=(fillx$DF/fillx$DMW)/(fillx$PF/fillx$PMW)
   fillx$POLFRAC=fillx$PMR/(fillx$PMR+fillx$MR1)
-  fillx$D1FRAC=fillx$MR1/(fillx$PMR+fillx$MR1)  
+  fillx$D1FRAC=fillx$MR1/(fillx$PMR+fillx$MR1)
   full=rbind(full,fillx)
 }
 colnames(full)=colnames(fill)
 full=full[-1,]
 
-write.table(full,file="formulations3test_db.csv",row.names = F,sep="\t",quote=F)
+write.table(full,file="db_formulations.csv",row.names = F,sep="\t",quote=F)
