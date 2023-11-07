9adbdb01296b9b5401445abf0313ff6c5c06cc60
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-07 17:27:24 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -105,11 +105,11 @@ if submit_button:
         #os.system(tune_DF)
       
         process2 = subprocess.Popen(["Rscript", "create.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        result2 = st.write(process2.communicate()
+        result2 = process2.communicate()
                            
     with st.spinner('CALCULATING PREDICTIONS (STEP 3 OF 3)...'):
         process3 = subprocess.Popen(["Rscript", "fgv.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        result3 = st.write(process3.communicate()
+        result3 = process3.communicate()
                 
         df = pd.read_csv(r'fin_results.csv',index_col=0)
 
