34afcee996faae7a97bdb5a94d614525f1e889f5
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-07 14:07:47 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -85,20 +85,20 @@ if submit_button:
           dfx = pd.DataFrame(columns=['NAME', "SMILES","MW"])
           dfx["NAME"]=NAMES
           dfx["SMILES"]=SMILES
-          dfx["MW"]=MW
+          dfx["MW"]=MW[0]
 
           dfx.to_csv("db_test.csv",index=False)
 
                              
        with st.spinner('CREATING FORMULATIONS (STEP 2 OF 6)...'):
           process1 = subprocess.Popen(["Rscript", "cxdb.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-          result1 = process1.communicate()
+          st.write(process1.communicate())
           os.system("sed -i -e 's/\"//g' db_formulations.csv")
           tune_DF=str("sed -i -e 's/10\\t8\t/10\\t"+set_DF+"\\t/g' db_formulations.csv")
           os.system(tune_DF)
       
           process2 = subprocess.Popen(["Rscript", "create.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-          result2 = process1.communicate()
+          st.write(process2.communicate())
                 
        with st.spinner('CALCULATING PREDICTIONS (STEP 6 OF 6)...'):
           process3 = subprocess.Popen(["Rscript", "fgv.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
