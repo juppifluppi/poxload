a05077e67fb12ef4099b6bfcf41ffa492c4e8646
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-07 14:16:48 +0000

    Update create.R

--- create.R
+++ create.R
@@ -4,12 +4,12 @@ smi=read.csv("db_test.csv")
 names=smi[,1]
 formulations=read.csv("db_formulations.csv",dec=".")
 
-formulations$Solvent[is.na(formulations$Solvent)]<-0
-formulations$Temperature[is.na(formulations$Temperature)]<-0
-formulations$Hydration[is.na(formulations$Hydration)]<-0
-formulations$VOL[is.na(formulations$VOL)]<-0
+#formulations$Solvent[is.na(formulations$Solvent)]<-0
+#formulations$Temperature[is.na(formulations$Temperature)]<-0
+#formulations$Hydration[is.na(formulations$Hydration)]<-0
+#formulations$VOL[is.na(formulations$VOL)]<-0
 
-formulations=na.omit(formulations)
+#formulations=na.omit(formulations)
 
 descriptors=descriptors[ , purrr::map_lgl(descriptors, is.numeric)]
 bbb=paste0(formulations$A1,"+",formulations$B,"+",formulations$A2,"+",formulations$D,"+",formulations$B2,"+",formulations$D2)
