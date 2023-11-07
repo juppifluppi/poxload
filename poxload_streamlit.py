e5e4a7ead159d8b6e34f93e02229a42aa2dac1e7
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-07 22:39:24 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -96,17 +96,14 @@ if submit_button:
         
 
         dfx.to_csv("db_test.csv",index=False)
-
-        tune_DF=str("sed -i -e 's/OOO/"+set_DF+"/g' db_smiles.csv")
-        os.system(tune_DF)
-    
+   
                             
     with st.spinner('CREATING FORMULATIONS (STEP 2 OF 3)...'):
         process1 = subprocess.Popen(["Rscript", "cxdb.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         st.write(process1.communicate())
         #os.system("sed -i -e 's/OOO//g' db_formulations.csv")
-        #tune_DF=str("sed -i -e 's/\\t10\\t8/\\t10\\t"+set_DF+"/g' db_formulations.csv")
-        #os.system(tune_DF)
+        tune_DF=str("sed -i -e 's/\\t10\\t8/\\t10\\t"+set_DF+"/g' db_formulations.csv")
+        os.system(tune_DF)
       
         process2 = subprocess.Popen(["Rscript", "create.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         st.write(process2.communicate())
