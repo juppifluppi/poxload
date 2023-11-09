b8f42e36688c5eebf37e22095a2a8e825f29b3ce
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-09 15:57:50 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -50,9 +50,8 @@ st.caption("""Input a [molecule SMILES code](https://pubchem.ncbi.nlm.nih.gov/ed
 
 with st.form(key='my_form_to_submit'):
     SMI = st.text_input('Enter SMILES code of drug to load (paclitaxel given as example)', 'CC1=C2[C@@]([C@]([C@H]([C@@H]3[C@]4([C@H](OC4)C[C@@H]([C@]3(C(=O)[C@@H]2OC(=O)C)C)O)OC(=O)C)OC(=O)c5ccccc5)(C[C@@H]1OC(=O)[C@H](O)[C@@H](NC(=O)c6ccccc6)c7ccccc7)O)(C)C') 
+    SMI2 = st.text_input('Potential co-formulation:', 'CCC') 
     submit_button = st.form_submit_button(label='Submit')
-    SMI2 = st.text_input('Enter SMILES code of additional drug', 'CCC') 
-    submit_button2 = st.form_submit_button(label='Submit co-formulation')
   
 if submit_button:   
     
