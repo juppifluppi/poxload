4dceb4f8dd7eb8bccb0fa0e96aba5b20110ead20
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-13 15:25:40 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -249,15 +249,10 @@ if submit_button:
 
         # Add scatter lines on the right side
         ax2 = ax.twinx()
-        sns.lineplot(x="DF", y="LE", hue="POL", data=df6, ax=ax2)
+        sns.lineplot(x="DF", y="LE", hue="POL", data=df6, ax=ax2,position=0.5)
         #sns.scatterplot(x="DF", y="SD", hue="POL", data=df5, ax=ax2, marker='x', s=10)
         ax2.set_ylabel("LE [%]")
         ax2.legend().set_visible(False)
-
-        
-        # Adjust x-axis ticks to align with the bar plot
-        ax.set_xticks(range(len(df3["DF"].unique())))
-        ax.set_xticklabels(df3["DF"].unique())
 
 
         # Adjust legend position
