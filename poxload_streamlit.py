8023c680aa00bc6523da240edcb66c5115951f84
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-13 17:17:32 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -358,10 +358,10 @@ if submit_button:
     
         with st.spinner('CALCULATING MIXTURE DESCRIPTORS (STEP 3 OF 4)...'):
             if choosemodel == 'RDK7-RF (recommended)':
-                process2 = subprocess.Popen(["Rscript", "create3.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                process2 = subprocess.Popen(["Rscript", "create.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                 st.write(process2.communicate())
             if choosemodel == 'Final (slower)':
-                process2 = subprocess.Popen(["Rscript", "create4.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                process2 = subprocess.Popen(["Rscript", "create2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                 result2 = process2.communicate()
                                
         with st.spinner('CALCULATING PREDICTIONS (STEP 4 OF 4)...'):
