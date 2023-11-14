02b5086296db3cf5421f26be405b65b0dc4632f3
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-14 23:29:06 +0000

    Update fgv5.R

--- fgv5.R
+++ fgv5.R
@@ -479,6 +479,7 @@ a=cbind(a,b)
 ck=t(apply(a, 1, function(u) table(factor(u, levels=c("X1","X0","AD")))))
 a=cbind(a,ck[,1])
 
+a$Time=rep(c(0,5,10,30),length(unique(a$DF)))
          
 colnames(a)=c("Time","D","DF","LC10","LC20","LC30","LC40","LE20","LE40","LE60","LE80","Passed")
 a=a[with(a, order(DF, D, Time)),]
