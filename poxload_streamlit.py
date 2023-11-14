47e787a9396ed5d25efbe092d27ba401aef73cd6
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-14 23:47:23 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -397,7 +397,7 @@ if submit_button:
                                        
             with st.spinner('CREATING FORMULATION DATABASE (STEP 2 OF 4)...'):
                 process1 = subprocess.Popen(["Rscript", "cxdb3.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                result1 = process1.communicate()
+                st.write(rocess1.communicate())
         
             with st.spinner('CALCULATING MIXTURE DESCRIPTORS (STEP 3 OF 4)...'):
                 if choosemodel == 'RDK7-RF (around 1 min)':
