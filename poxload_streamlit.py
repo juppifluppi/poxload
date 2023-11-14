a1ff8960b039b1feab3f96b94a2e4410e6f2bd32
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-11-14 23:25:45 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -406,7 +406,7 @@ if submit_button:
     
                 if choosemodel == 'Final models (around 7 min)':
                     process2 = subprocess.Popen(["Rscript", "create2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                    result2 = process2.communicate()
+                    st.write(process2.communicate())
             
             with st.spinner('CALCULATING PREDICTIONS (STEP 4 OF 4)...'):
                 if choosemodel == 'RDK7-RF (around 1 min)':
