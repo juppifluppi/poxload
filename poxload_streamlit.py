2653a63d4cc968025517a933226e40e4e8be7282
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2023-12-19 16:05:39 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -212,8 +212,8 @@ if submit_button:
                         SDc = ((df2["DF"])*((df2["LE"])/100))
                         #SDc2 = (((df2["LC"]/100)*(10))/(1-(df2["LC"]/100)))
                         SDc2 = (((df2["LC"]/100)*(-1)*10)/((df2["LC"]/100)-1))
-                        #SDcx = ((SDc+SDc2)/2)
-                        SDcx = pd.concat([SDc, SDc2], axis=1).min(axis=1)
+                        SDcx = ((SDc+SDc2)/2)
+                        #SDcx = pd.concat([SDc, SDc2], axis=1).min(axis=1)
                         #SDcx = pd.concat([SDc, SDc2], axis=1).max(axis=1)
                     
             
@@ -424,8 +424,8 @@ if submit_button:
                     SDc = ((df2["DF"])*((df2["LE"])/100))
                     #Dc2 = (((df2["LC"]/100)*10)/(1-(df2["LC"]/100)))
                     SDc2 = (((df2["LC"]/100)*(-1)*10)/((df2["LC"]/100)-1))
-                    #SDcx = ((SDc+SDc2)/2)
-                    SDcx = pd.concat([SDc, SDc2], axis=1).min(axis=1)
+                    SDcx = ((SDc+SDc2)/2)
+                    #SDcx = pd.concat([SDc, SDc2], axis=1).min(axis=1)
                     #SDcx = pd.concat([SDc, SDc2], axis=1).max(axis=1)
                     
                     if len(SMI2) > 2:
