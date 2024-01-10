b52dda6212f11b3f00d560325549fb6c192786a5
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-01-10 20:20:05 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -62,7 +62,7 @@ The formulations are assumed to be made via thin-film hydration using ethanol as
  Version 1.0 (19.12.2023)""")
  
 
-    SMI = st.text_input('Enter [SMILES code](https://pubchem.ncbi.nlm.nih.gov//edit3/index.html) of drug to load', 'CC1=C2[C@@]([C@]([C@H]([C@@H]3[C@]4([C@H](OC4)C[C@@H]([C@]3(C(=O)[C@@H]2OC(=O)C)C)O)OC(=O)C)OC(=O)c5ccccc5)(C[C@@H]1OC(=O)[C@H](O)[C@@H](NC(=O)c6ccccc6)c7ccccc7)O)(C)C') 
+    SMI = st.text_input('Enter [SMILES code](https://pubchem.ncbi.nlm.nih.gov//edit3/index.html) of drug to load', '') 
     
     on = st.toggle('Use drawn structure',key="13")
     with st.expander("SMILES editor"):
