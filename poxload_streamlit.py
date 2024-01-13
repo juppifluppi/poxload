5b871ae0cde61d92eabc985d83fab3e536bc397a
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-01-13 10:37:16 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -102,17 +102,18 @@ with st.form(key='my_form_to_submit'):
         st.write("Alternative computations")
         
         on3 = st.toggle('Perform batch calculation',key="16")    
-            col1, col2 = st.columns(2)
-            with col1:
-                st.write("Names of compounds")
-                NAMESx = st.text_area(label="Input names of compounds separated by linebreaks",key="17")
-            with col2:
-                st.write("SMILES codes")
-                SMILESx = st.text_area(label="Input SMILES of compounds separated by linebreaks",key="18")
+
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write("Names of compounds")
+            NAMESx = st.text_area(label="Input names of compounds separated by linebreaks",key="17")
+        with col2:
+            st.write("SMILES codes")
+            SMILESx = st.text_area(label="Input SMILES of compounds separated by linebreaks",key="18")
     
         on4 = st.toggle('Predict long-term storage',key="19")    
-            options2 = st.selectbox('Select polymer for 0-30 days storage:',
-                             ("A-nBuOx-A","A-nBuOzi-A","A-nPrOx-A","A-nPrOzi-A","A-cPrOx-A","A-cPrOzi-A","A-iPrOx-A","A-iPrOzi-A","A-cPrMeOx-A","A-cPrMeOzi-A","A-iBuOx-A","A-iBuOzi-A","A-sBuOx-A","A-sBuOzi-A","A-PentOx-A","A*-nPrOzi-A*","A*-nBuOx-A*","A-BzOx-A","A-BzOzi-A","A-PhOx-A","A-PhOzi-A","A-EtHepOx-A","A-EtHepOzi-A","A-nNonOx-A","A-nNonOzi-A"))
+        options2 = st.selectbox('Select polymer for 0-30 days storage:',
+                         ("A-nBuOx-A","A-nBuOzi-A","A-nPrOx-A","A-nPrOzi-A","A-cPrOx-A","A-cPrOzi-A","A-iPrOx-A","A-iPrOzi-A","A-cPrMeOx-A","A-cPrMeOzi-A","A-iBuOx-A","A-iBuOzi-A","A-sBuOx-A","A-sBuOzi-A","A-PentOx-A","A*-nPrOzi-A*","A*-nBuOx-A*","A-BzOx-A","A-BzOzi-A","A-PhOx-A","A-PhOzi-A","A-EtHepOx-A","A-EtHepOzi-A","A-nNonOx-A","A-nNonOzi-A"))
 
     emoji = '💊'
     label = ' Formulate!'
