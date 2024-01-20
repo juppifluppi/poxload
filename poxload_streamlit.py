f3c66841b6cbc989eea0311e7c83f63df26a02f1
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-01-20 17:15:51 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -375,7 +375,7 @@ if submit_button:
                         with NamedTemporaryFile(delete=False, suffix=".png") as tmpfile:
                                 fig.savefig(tmpfile.name)
                                 pdf.image(tmpfile.name, 10, 10, 200, 100)
-                    html = create_download_link(pdf.output(dest="S").encode("latin-1"), "testfile")
+                    html = create_download_link(pdf.output(dest="S"), "test")
                     st.markdown(html, unsafe_allow_html=True)
 
 
