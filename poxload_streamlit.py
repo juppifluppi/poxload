df200a53e83b2f8b29e7e267fe69537be5f2adef
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-01-23 22:41:27 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -196,18 +196,22 @@ if submit_button:
                 if choosemodel == 'RDK7-RF [holdout AUC = 0.88, up to ~1 min]':
                     process2 = subprocess.Popen(["Rscript", "create_mixtures.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     result2 = process2.communicate()
+                    process2.communicate()
     
                 if choosemodel == 'Final models [holdout AUC = 0.91, up to ~7 min]':
                     process2 = subprocess.Popen(["Rscript", "create_mixtures2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     result2 = process2.communicate()
+                    process2.communicate()
             
             with st.spinner('CALCULATING PREDICTIONS (STEP 4 OF 4)...'):
                 if choosemodel == 'RDK7-RF [holdout AUC = 0.88, up to ~1 min]':
                     process3 = subprocess.Popen(["Rscript", "predict.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     result3 = process3.communicate()
+                    process3.communicate()
                 if choosemodel == 'Final models [holdout AUC = 0.91, up to ~7 min]':
                     process3 = subprocess.Popen(["Rscript", "predict2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     result3 = process3.communicate()
+                    process3.communicate()
                 
                 df2 = pd.read_csv(r'fin_results2.csv')
                 df2 = df2.rename(columns={0: "POL", 1: "DF", 2: "LC", 3: "LE"})
@@ -642,4 +646,8 @@ if submit_button:
 #except:
 #    st.write("Something went wrong. Cannot parse molecules! Please verify your structures.")  
 
-os.system("rm db_formulations.csv db_test.csv options.csv descriptors.csv fin_results.csv fin_results2.csv testformulations.dat")
+for es in ["db_formulations.csv","db_test.csv","options.csv","descriptors.csv","fin_results.csv","fin_results2.csv","testformulations.dat"]:
+    try:
+        os.remove(es)
+    except:
+        pass
