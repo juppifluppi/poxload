f55180625fa162151ff91dfaab95c4e431c0fd7c
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-01-23 22:44:56 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -195,22 +195,19 @@ if submit_button:
             with st.spinner('CALCULATING MIXTURE DESCRIPTORS (STEP 3 OF 4)...'):
                 if choosemodel == 'RDK7-RF [holdout AUC = 0.88, up to ~1 min]':
                     process2 = subprocess.Popen(["Rscript", "create_mixtures.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                    result2 = process2.communicate()
                     process2.communicate()
     
                 if choosemodel == 'Final models [holdout AUC = 0.91, up to ~7 min]':
                     process2 = subprocess.Popen(["Rscript", "create_mixtures2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                    result2 = process2.communicate()
                     process2.communicate()
             
             with st.spinner('CALCULATING PREDICTIONS (STEP 4 OF 4)...'):
                 if choosemodel == 'RDK7-RF [holdout AUC = 0.88, up to ~1 min]':
                     process3 = subprocess.Popen(["Rscript", "predict.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                    result3 = process3.communicate()
                     process3.communicate()
+
                 if choosemodel == 'Final models [holdout AUC = 0.91, up to ~7 min]':
                     process3 = subprocess.Popen(["Rscript", "predict2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-                    result3 = process3.communicate()
                     process3.communicate()
                 
                 df2 = pd.read_csv(r'fin_results2.csv')
