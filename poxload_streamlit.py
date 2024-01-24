1f6447e90eca5c396b8d332e2b4314c735a9d483
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-01-24 12:12:45 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -192,23 +192,29 @@ if submit_button:
             with st.spinner('CREATING FORMULATION DATABASE (STEP 2 OF 4)...'):
                 process1 = subprocess.Popen(["Rscript", "create_formulations_temp.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                 result1 = process1.communicate()
+                st.write(result1)
         
             with st.spinner('CALCULATING MIXTURE DESCRIPTORS (STEP 3 OF 4)...'):
                 if choosemodel == 'RDK7-RF (around 1 min)':
                     process2 = subprocess.Popen(["Rscript", "create_mixtures.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     result2 = process2.communicate()
+                    st.write(result2)
     
                 if choosemodel == 'Final models (around 7 min)':
                     process2 = subprocess.Popen(["Rscript", "create_mixtures2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     result2 = process2.communicate()
+                    st.write(result2)
             
             with st.spinner('CALCULATING PREDICTIONS (STEP 4 OF 4)...'):
                 if choosemodel == 'RDK7-RF (around 1 min)':
                     process3 = subprocess.Popen(["Rscript", "predict.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     result3 = process3.communicate()
+                    st.write(result3)
+                    
                 if choosemodel == 'Final models (around 7 min)':
                     process3 = subprocess.Popen(["Rscript", "predict2.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
                     result3 = process3.communicate()
+                    st.write(result3)
                 
                 df2 = pd.read_csv(r'fin_results2.csv')
                 df2 = df2.rename(columns={0: "POL", 1: "DF", 2: "LC", 3: "LE"})
