60a98ffeb4baa43686dbd04c594767358f37f7df
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-01-24 22:35:45 +0000

    Delete rpackages.R

--- rpackages.R
+++
@@ -1,9 +0,0 @@
-packages <- c("caret","randomForest","kernlab","proxy","xgboost")
-install_if_not_installed <- function(package) {
-  if (!require(package, character.only = TRUE)) {
-    install.packages(package, repos = "http://cran.rstudio.com/", lib="/usr/lib/R/library")
-  }
-
-# Install the packages
-sapply(packages, install_if_not_installed)
-}
