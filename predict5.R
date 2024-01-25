a1dc3007f9abab708340f07d5549ffb6bb67e16d
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-01-25 12:10:40 +0000

    Update predict5.R

--- predict5.R
+++ predict5.R
@@ -1,40 +1,3 @@
-#05
-thr05_1=12.713983
-thr05_2=12.540893
-thr05_3=10.051199
-thr05_4=6.991115
-thr05_5=13.183467
-thr05_6=13.243743
-thr05_7=12.684053
-thr05_8=11.724952
-#1
-thr1_1=16.439715
-thr1_2=16.183039
-thr1_3=13.146608
-thr1_4=9.215794
-thr1_5=17.161885
-thr1_6=17.220897
-thr1_7=16.592384
-thr1_8=15.094306
-#2
-thr2_1=23.89118
-thr2_2=23.46733
-thr2_3=19.33742
-thr2_4=13.66515
-thr2_5=25.11872
-thr2_6=25.17520
-thr2_7=24.40905
-thr2_8=21.83301
-#3
-thr3_1=31.34264
-thr3_2=30.75162
-thr3_3=25.52824
-thr3_4=18.11451
-thr3_5=33.07556
-thr3_6=33.12951
-thr3_7=32.22571
-thr3_8=28.57172
-#4
 thr4_1=38.79411
 thr4_2=38.03592
 thr4_3=31.71906
@@ -67,26 +30,6 @@ m8=model
 
 af=read.csv("testformulations.dat",check.names = F)
 afx=af
-#afx$Time=rep(c(0,5,10,30),length(unique(a$DF)))
-
-#ui=m1$trainingData[-1]
-#afx2=afx[,colnames(afx)%in%colnames(ui)]
-#Missing <- setdiff(colnames(ui), colnames(afx2))
-#afx2[Missing] <- 0
-#afx2 <- afx2[colnames(ui)]
-#is.na(afx2)<-sapply(afx2, is.infinite)
-#afx2[is.na(afx2)]<-0
-#
-#preproc <- preProcess(ui, method=c("center","scale","YeoJohnson"))
-#uix <- predict(preproc, newdata = ui)
-#z1=c()
-#for(huh in 1:nrow(afx2)){
-#  scaled.new <- predict(preproc, newdata = afx2[huh,])
-#  aggg=as.matrix(dist(rbind(scaled.new,uix)))[1,-1]
-#  aggg=abs(sort(aggg)[1:15])
-#  aggg=mean(aggg,na.rm=T)
-#  z1=append(z1,aggg)
-#}
 
 ui <- m1$trainingData[-1]
 afx2 <- afx[, colnames(afx) %in% colnames(ui)]
@@ -100,14 +43,8 @@ preproc <- preProcess(ui, method = c("center", "scale", "YeoJohnson"))
 uix <- predict(preproc, newdata = ui)
 scaled_new <- predict(preproc, newdata = afx2)
 
-# Use proxy package for efficient distance calculation
 distances <- proxy::dist(proxy::as.matrix(scaled_new), proxy::as.matrix(uix))
 
-## Extract the distances for each row in afx2 and calculate the mean
-#z1 <- sapply(1:nrow(afx2), function(huh) {
-#  aggg = abs(sort(distances[huh, distances[huh, ] > 0])[1:15])
-#  mean(aggg, na.rm = TRUE)
-#})
 agggsx=c()
 for(uj in 1:nrow(afx2)){
   agggsx = append(agggsx,mean(abs(sort(distances[uj, distances[uj, ] > 0])[1:15]),na.rm=T))
@@ -131,25 +68,6 @@ a=cbind(a,b)
 
 
 
-#ui=m2$trainingData[-1]
-#afx2=afx[,colnames(afx)%in%colnames(ui)]
-#Missing <- setdiff(colnames(ui), colnames(afx2))
-#afx2[Missing] <- 0
-#afx2 <- afx2[colnames(ui)]
-#is.na(afx2)<-sapply(afx2, is.infinite)
-#afx2[is.na(afx2)]<-0
-#
-#preproc <- preProcess(ui, method=c("center","scale","YeoJohnson"))
-#uix <- predict(preproc, newdata = ui)
-#z1=c()
-#for(huh in 1:nrow(afx2)){
-#  scaled.new <- predict(preproc, newdata = afx2[huh,])
-#  aggg=as.matrix(dist(rbind(scaled.new,uix)))[1,-1]
-#  aggg=abs(sort(aggg)[1:15])
-#  aggg=mean(aggg,na.rm=T)
-#  z1=append(z1,aggg)
-#}
-
 ui <- m2$trainingData[-1]
 afx2 <- afx[, colnames(afx) %in% colnames(ui)]
 Missing <- setdiff(colnames(ui), colnames(afx2))
@@ -162,14 +80,8 @@ preproc <- preProcess(ui, method = c("center", "scale", "YeoJohnson"))
 uix <- predict(preproc, newdata = ui)
 scaled_new <- predict(preproc, newdata = afx2)
 
-# Use proxy package for efficient distance calculation
 distances <- proxy::dist(proxy::as.matrix(scaled_new), proxy::as.matrix(uix))
 
-## Extract the distances for each row in afx2 and calculate the mean
-#z1 <- sapply(1:nrow(afx2), function(huh) {
-#  aggg = abs(sort(distances[huh, distances[huh, ] > 0])[1:15])
-#  mean(aggg, na.rm = TRUE)
-#})
 agggsx=c()
 for(uj in 1:nrow(afx2)){
   agggsx = append(agggsx,mean(abs(sort(distances[uj, distances[uj, ] > 0])[1:15]),na.rm=T))
@@ -188,25 +100,6 @@ a=cbind(a,b)
 
 
 
-#ui=m3$trainingData[-1]
-#afx2=afx[,colnames(afx)%in%colnames(ui)]
-#Missing <- setdiff(colnames(ui), colnames(afx2))
-#afx2[Missing] <- 0
-#afx2 <- afx2[colnames(ui)]
-#is.na(afx2)<-sapply(afx2, is.infinite)
-#afx2[is.na(afx2)]<-0
-#
-#preproc <- preProcess(ui, method=c("center","scale","YeoJohnson"))
-#uix <- predict(preproc, newdata = ui)
-#z1=c()
-#for(huh in 1:nrow(afx2)){
-#  scaled.new <- predict(preproc, newdata = afx2[huh,])
-#  aggg=as.matrix(dist(rbind(scaled.new,uix)))[1,-1]
-#  aggg=abs(sort(aggg)[1:15])
-#  aggg=mean(aggg,na.rm=T)
-#  z1=append(z1,aggg)
-#}
-
 ui <- m3$trainingData[-1]
 afx2 <- afx[, colnames(afx) %in% colnames(ui)]
 Missing <- setdiff(colnames(ui), colnames(afx2))
@@ -219,14 +112,8 @@ preproc <- preProcess(ui, method = c("center", "scale", "YeoJohnson"))
 uix <- predict(preproc, newdata = ui)
 scaled_new <- predict(preproc, newdata = afx2)
 
-# Use proxy package for efficient distance calculation
 distances <- proxy::dist(proxy::as.matrix(scaled_new), proxy::as.matrix(uix))
 
-## Extract the distances for each row in afx2 and calculate the mean
-#z1 <- sapply(1:nrow(afx2), function(huh) {
-#  aggg = abs(sort(distances[huh, distances[huh, ] > 0])[1:15])
-#  mean(aggg, na.rm = TRUE)
-#})
 agggsx=c()
 for(uj in 1:nrow(afx2)){
   agggsx = append(agggsx,mean(abs(sort(distances[uj, distances[uj, ] > 0])[1:15]),na.rm=T))
@@ -243,25 +130,6 @@ a=cbind(a,b)
 
 
 
-#ui=m4$trainingData[-1]
-#afx2=afx[,colnames(afx)%in%colnames(ui)]
-#Missing <- setdiff(colnames(ui), colnames(afx2))
-#afx2[Missing] <- 0
-#afx2 <- afx2[colnames(ui)]
-#is.na(afx2)<-sapply(afx2, is.infinite)
-#afx2[is.na(afx2)]<-0
-#
-#preproc <- preProcess(ui, method=c("center","scale","YeoJohnson"))
-#uix <- predict(preproc, newdata = ui)
-#z1=c()
-#for(huh in 1:nrow(afx2)){
-#  scaled.new <- predict(preproc, newdata = afx2[huh,])
-#  aggg=as.matrix(dist(rbind(scaled.new,uix)))[1,-1]
-#  aggg=abs(sort(aggg)[1:15])
-#  aggg=mean(aggg,na.rm=T)
-#  z1=append(z1,aggg)
-#}
-
 ui <- m4$trainingData[-1]
 afx2 <- afx[, colnames(afx) %in% colnames(ui)]
 Missing <- setdiff(colnames(ui), colnames(afx2))
@@ -274,14 +142,8 @@ preproc <- preProcess(ui, method = c("center", "scale", "YeoJohnson"))
 uix <- predict(preproc, newdata = ui)
 scaled_new <- predict(preproc, newdata = afx2)
 
-# Use proxy package for efficient distance calculation
 distances <- proxy::dist(proxy::as.matrix(scaled_new), proxy::as.matrix(uix))
 
-## Extract the distances for each row in afx2 and calculate the mean
-#z1 <- sapply(1:nrow(afx2), function(huh) {
-#  aggg = abs(sort(distances[huh, distances[huh, ] > 0])[1:15])
-#  mean(aggg, na.rm = TRUE)
-#})
 agggsx=c()
 for(uj in 1:nrow(afx2)){
   agggsx = append(agggsx,mean(abs(sort(distances[uj, distances[uj, ] > 0])[1:15]),na.rm=T))
@@ -296,25 +158,6 @@ b[gzy==FALSE]="AD"
 a=cbind(a,b)
 
 
-#ui=m5$trainingData[-1]
-#afx2=afx[,colnames(afx)%in%colnames(ui)]
-#Missing <- setdiff(colnames(ui), colnames(afx2))
-#afx2[Missing] <- 0
-#afx2 <- afx2[colnames(ui)]
-#is.na(afx2)<-sapply(afx2, is.infinite)
-#afx2[is.na(afx2)]<-0
-#
-#preproc <- preProcess(ui, method=c("center","scale","YeoJohnson"))
-#uix <- predict(preproc, newdata = ui)
-#z1=c()
-#for(huh in 1:nrow(afx2)){
-#  scaled.new <- predict(preproc, newdata = afx2[huh,])
-#  aggg=as.matrix(dist(rbind(scaled.new,uix)))[1,-1]
-#  aggg=abs(sort(aggg)[1:15])
-#  aggg=mean(aggg,na.rm=T)
-#  z1=append(z1,aggg)
-#}
-
 ui <- m5$trainingData[-1]
 afx2 <- afx[, colnames(afx) %in% colnames(ui)]
 Missing <- setdiff(colnames(ui), colnames(afx2))
@@ -327,14 +170,8 @@ preproc <- preProcess(ui, method = c("center", "scale", "YeoJohnson"))
 uix <- predict(preproc, newdata = ui)
 scaled_new <- predict(preproc, newdata = afx2)
 
-# Use proxy package for efficient distance calculation
 distances <- proxy::dist(proxy::as.matrix(scaled_new), proxy::as.matrix(uix))
 
-## Extract the distances for each row in afx2 and calculate the mean
-#z1 <- sapply(1:nrow(afx2), function(huh) {
-#  aggg = abs(sort(distances[huh, distances[huh, ] > 0])[1:15])
-#  mean(aggg, na.rm = TRUE)
-#})
 agggsx=c()
 for(uj in 1:nrow(afx2)){
   agggsx = append(agggsx,mean(abs(sort(distances[uj, distances[uj, ] > 0])[1:15]),na.rm=T))
@@ -351,25 +188,6 @@ a=cbind(a,b)
 
 
 
-#ui=m6$trainingData[-1]
-#afx2=afx[,colnames(afx)%in%colnames(ui)]
-#Missing <- setdiff(colnames(ui), colnames(afx2))
-#afx2[Missing] <- 0
-#afx2 <- afx2[colnames(ui)]
-#is.na(afx2)<-sapply(afx2, is.infinite)
-#afx2[is.na(afx2)]<-0
-#
-#preproc <- preProcess(ui, method=c("center","scale","YeoJohnson"))
-#uix <- predict(preproc, newdata = ui)
-#z1=c()
-#for(huh in 1:nrow(afx2)){
-#  scaled.new <- predict(preproc, newdata = afx2[huh,])
-#  aggg=as.matrix(dist(rbind(scaled.new,uix)))[1,-1]
-#  aggg=abs(sort(aggg)[1:15])
-#  aggg=mean(aggg,na.rm=T)
-#  z1=append(z1,aggg)
-#}
-
 ui <- m6$trainingData[-1]
 afx2 <- afx[, colnames(afx) %in% colnames(ui)]
 Missing <- setdiff(colnames(ui), colnames(afx2))
@@ -382,14 +200,8 @@ preproc <- preProcess(ui, method = c("center", "scale", "YeoJohnson"))
 uix <- predict(preproc, newdata = ui)
 scaled_new <- predict(preproc, newdata = afx2)
 
-# Use proxy package for efficient distance calculation
 distances <- proxy::dist(proxy::as.matrix(scaled_new), proxy::as.matrix(uix))
 
-## Extract the distances for each row in afx2 and calculate the mean
-#z1 <- sapply(1:nrow(afx2), function(huh) {
-#  aggg = abs(sort(distances[huh, distances[huh, ] > 0])[1:15])
-#  mean(aggg, na.rm = TRUE)
-#})
 agggsx=c()
 for(uj in 1:nrow(afx2)){
   agggsx = append(agggsx,mean(abs(sort(distances[uj, distances[uj, ] > 0])[1:15]),na.rm=T))
@@ -406,25 +218,6 @@ a=cbind(a,b)
 
 
 
-#ui=m7$trainingData[-1]
-#afx2=afx[,colnames(afx)%in%colnames(ui)]
-#Missing <- setdiff(colnames(ui), colnames(afx2))
-#afx2[Missing] <- 0
-#afx2 <- afx2[colnames(ui)]
-#is.na(afx2)<-sapply(afx2, is.infinite)
-#afx2[is.na(afx2)]<-0
-#
-#preproc <- preProcess(ui, method=c("center","scale","YeoJohnson"))
-#uix <- predict(preproc, newdata = ui)
-#z1=c()
-#for(huh in 1:nrow(afx2)){
-#  scaled.new <- predict(preproc, newdata = afx2[huh,])
-#  aggg=as.matrix(dist(rbind(scaled.new,uix)))[1,-1]
-#  aggg=abs(sort(aggg)[1:15])
-#  aggg=mean(aggg,na.rm=T)
-#  z1=append(z1,aggg)
-#}
-
 ui <- m7$trainingData[-1]
 afx2 <- afx[, colnames(afx) %in% colnames(ui)]
 Missing <- setdiff(colnames(ui), colnames(afx2))
@@ -437,14 +230,8 @@ preproc <- preProcess(ui, method = c("center", "scale", "YeoJohnson"))
 uix <- predict(preproc, newdata = ui)
 scaled_new <- predict(preproc, newdata = afx2)
 
-# Use proxy package for efficient distance calculation
 distances <- proxy::dist(proxy::as.matrix(scaled_new), proxy::as.matrix(uix))
 
-## Extract the distances for each row in afx2 and calculate the mean
-#z1 <- sapply(1:nrow(afx2), function(huh) {
-#  aggg = abs(sort(distances[huh, distances[huh, ] > 0])[1:15])
-#  mean(aggg, na.rm = TRUE)
-#})
 agggsx=c()
 for(uj in 1:nrow(afx2)){
   agggsx = append(agggsx,mean(abs(sort(distances[uj, distances[uj, ] > 0])[1:15]),na.rm=T))
@@ -460,25 +247,6 @@ b[gzy==FALSE]="AD"
 a=cbind(a,b)
 
 
-#ui=m8$trainingData[-1]
-#afx2=afx[,colnames(afx)%in%colnames(ui)]
-#Missing <- setdiff(colnames(ui), colnames(afx2))
-#afx2[Missing] <- 0
-#afx2 <- afx2[colnames(ui)]
-#is.na(afx2)<-sapply(afx2, is.infinite)
-#afx2[is.na(afx2)]<-0
-#
-#preproc <- preProcess(ui, method=c("center","scale","YeoJohnson"))
-#uix <- predict(preproc, newdata = ui)
-#z1=c()
-#for(huh in 1:nrow(afx2)){
-#  scaled.new <- predict(preproc, newdata = afx2[huh,])
-#  aggg=as.matrix(dist(rbind(scaled.new,uix)))[1,-1]
-#  aggg=abs(sort(aggg)[1:15])
-#  aggg=mean(aggg,na.rm=T)
-#  z1=append(z1,aggg)
-#}
-
 ui <- m8$trainingData[-1]
 afx2 <- afx[, colnames(afx) %in% colnames(ui)]
 Missing <- setdiff(colnames(ui), colnames(afx2))
@@ -491,14 +259,8 @@ preproc <- preProcess(ui, method = c("center", "scale", "YeoJohnson"))
 uix <- predict(preproc, newdata = ui)
 scaled_new <- predict(preproc, newdata = afx2)
 
-# Use proxy package for efficient distance calculation
 distances <- proxy::dist(proxy::as.matrix(scaled_new), proxy::as.matrix(uix))
 
-## Extract the distances for each row in afx2 and calculate the mean
-#z1 <- sapply(1:nrow(afx2), function(huh) {
-#  aggg = abs(sort(distances[huh, distances[huh, ] > 0])[1:15])
-#  mean(aggg, na.rm = TRUE)
-#})
 agggsx=c()
 for(uj in 1:nrow(afx2)){
   agggsx = append(agggsx,mean(abs(sort(distances[uj, distances[uj, ] > 0])[1:15]),na.rm=T))
@@ -516,7 +278,7 @@ a=cbind(a,b)
 ck=t(apply(a, 1, function(u) table(factor(u, levels=c("X1","X0","AD")))))
 a=cbind(a,ck[,1])
 
-#a[,1]=rep(c(0,5,10,30),length(unique(a[,2])))
+
          
 colnames(a)=c("Time","DF","LC10","LC20","LC30","LC40","LE20","LE40","LE60","LE80","Passed")
 a=a[with(a, order(DF, Time)),]
@@ -569,32 +331,12 @@ get_last_non_zero <- function(vec) {
   return(last_non_zero)
 }
 
-#get_last_non_zero <- function(vec) {
-#  non_zero_elements <- vec[vec != 0]
-#  if (length(non_zero_elements) == 0) {
-#    return(0)  # If there are no non-zero elements, return 0
-#  } else {
-#    return(tail(non_zero_elements, 1))
-#  }
-#}
 
-# Apply the function to each row of the dataframe
 last_non_zero_elements <- apply(fg1, 1, get_last_non_zero)
-
-# Create a new dataframe with the last non-zero elements
 fg1 <- data.frame(LastNonZero = last_non_zero_elements)
-
-# Apply the function to each row of the dataframe
 last_non_zero_elements <- apply(fg2, 1, get_last_non_zero)
-
-# Create a new dataframe with the last non-zero elements
 fg2 <- data.frame(LastNonZero = last_non_zero_elements)
           
 a=cbind(a[,1],a[,2],fg1,fg2)
 colnames(a)=c("Time","DF","LC","LE")
 write.csv(a,"fin_results2.csv",row.names=F)
-
-#a=cbind(a[,1],a[,2],(as.numeric(a[,4])/100)*as.numeric(a[,2]))
-#colnames(a)=c("POL","DF","SD")
-#write.csv(a,"fin_results3.csv",row.names=F)
-
