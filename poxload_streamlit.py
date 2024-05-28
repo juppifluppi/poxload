8de5087afd435f408d1655c035ae2abafa3ac462
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2024-05-28 23:45:28 +0000

    Update poxload_streamlit.py

--- poxload_streamlit.py
+++ poxload_streamlit.py
@@ -62,7 +62,7 @@ with st.form(key='my_form_to_submit'):
         st.caption("""Final prediction models provided the highest accuracy on external data, but require initialization of the xgboost package.
         RDK7-RF models showed slightly lower accuracy, but provide faster computation and a larger applicability (suitable for drugs where final models show "AD" classifications).""") 
         
-        st.caption("""Details can be found in our [preprint](https://doi.org/10.26434/chemrxiv-2024-l5kvc).""")
+        st.caption("""Details can be found in our [publication](https://pubs.acs.org/doi/10.1021/acs.molpharmaceut.4c00086) or the [open-access preprint](https://doi.org/10.26434/chemrxiv-2024-l5kvc) of it.""")
         
         st.caption("""The software is hosted and downloadable as command-line tool at our [github page](https://github.com/juppifluppi/poxload), licensed under MIT.""")
  
