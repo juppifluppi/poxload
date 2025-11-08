559b0da43fded55de3e91e28ba5e9cab9ecbbdfc
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2025-11-08 21:04:53 +0100

    Update redirect.py

--- redirect.py
+++ redirect.py
@@ -4,10 +4,5 @@ st.set_page_config(page_title="Redirecting...", page_icon="⏩")
 
 st.markdown("""
 ## 🚀 We've moved!
-Please visit our new [👉 address](46.62.247.214:8501)!
+Please visit our new 👉 [address](46.62.247.214:8501)!
 """)
-
-# Optional: automatic redirect after 3 seconds
-st.markdown("""
-<meta http-equiv="refresh" content="3; url=46.62.247.214:8501" />
-""", unsafe_allow_html=True)
