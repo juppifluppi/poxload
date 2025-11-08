d37074a662917bfd3a70f4dee32d169ac0fbff22
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2025-11-08 21:30:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-__version__ = "1.0"
+__version__ = "1.2"
 
 # Load README
 with open('README.md', encoding='utf-8') as f:
@@ -8,7 +8,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='poxload',
-    version='1.1',
+    version='1.2',
     author='Josef Kehrein',
     author_email='josef.kehrein@helsinki.fi',
     license='MIT',
