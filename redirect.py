f60fecf66cd9cb1c77ffab3ab2461d7f4aebec3b
juppifluppi <117020072+juppifluppi@users.noreply.github.com>
2025-11-08 21:14:31 +0100

    Update redirect.py

--- redirect.py
+++ redirect.py
@@ -4,3 +4,9 @@ st.markdown("""
 ## 🚀 We've moved!
 Please visit our new 👉 [address](http://46.62.247.214:8501)!
 """)
+
+
+# Optional: automatic redirect after 3 seconds
+st.markdown("""
+<meta http-equiv="refresh" content="3; url=http://46.62.247.214:8501" />
+""", unsafe_allow_html=True)
